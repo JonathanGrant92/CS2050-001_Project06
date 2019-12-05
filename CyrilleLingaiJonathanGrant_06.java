d02c3a1560d3366675de6e22c03e1128531ad149
JonathanGrant92 <45248168+JonathanGrant92@users.noreply.github.com>
2019-12-05 10:34:50 -0700

    Revised the Source File & Updated IO files.
    Refactored the quick sort and main methods.
    Edited comments including java doc comments.
    Added a header comment to the program.
    Reformatted comments and code for consistency.

--- CyrilleLingaiJonathanGrant_06.java
+++ CyrilleLingaiJonathanGrant_06.java
@@ -1,5 +1,21 @@
 package CyrilleLingaiJonathanGrant_06;
 
+/*
+ * ComputerScience_02_01
+ * Write the student and gradeItem classes to track students and their grades 
+ * for all enrolled courses at MSU Denver. 
+ * JonathanGrant & CyrilleLingai
+ * Integer Lists Sorting Algorithms Program_06
+ * Windows 10 Eclipse IDE JRE 1.8
+ * 
+ * Interlude: a temporary amusement or source of entertainment that contrasts with 
+ * 				what goes before or after.
+ * 
+ * "Computer Science is no more about computers than astronomy is about
+ *  telescopes."
+ * Edsger W. Dijkstra, born March 11, 1930. died August 6, 2002.
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -7,7 +23,6 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * 
  * Sort lists of integers by shell sorting and quick sorting algorithms.
  * 
  * @author Cyrille Lingai, Grant Jonathan.
@@ -16,30 +31,35 @@ import java.util.Scanner;
 
 public class CyrilleLingaiJonathanGrant_06 {
 	
-	final static String inputFileName = "2050 Project 06_Input.txt";
+	// Declaring class variables.
+	
+	private final static String inputFileName = "2050 Project 06_Input.txt";
 									// The file of random integers.
+	private final static int sequenceLength = 100;
+									// The length of the list of integers.
 	
 	/**
+	 * Main execution of program to scan input files, sort lists of integers, 
+	 * and write those lists to independent files.
 	 * 
-	 * 
-	 * @param		args.
+	 * @param		args					The IO streams for reading and writing files.
 	 * @throws		FileNotFoundException 	If the file is not found.
 	 * @throws		IOException				If there is not to a writable file.		
 	 */
+	
 	public static void main(String[] args) {
 		
 		// Declaring local variables.
 		
 		int lineNumber = 0;			// Track the line an expression is scanned from.
 		int fileNumber = 1; 		// The file number to identify and scan files.
-		final int sequenceLength = 100;	// The length of the list of integers.
-	
+		
 		int[] quickSortedArray = new int[sequenceLength];
 									// The quick sorted integers to write to file.
 		int[] shellSortedArray = new int[sequenceLength];	
 									// The shell sorted integers to write to file.
 		
-		String outputFileName = "2050 Project 06_Output0X.txt";
+		String outputFileName = "2050 Project 06_OutputX.txt";
 									// The file of corresponding sorted integers.
 		
 		File inputFile = new File(inputFileName);	// The tool for openning files.
@@ -52,7 +72,8 @@ public class CyrilleLingaiJonathanGrant_06 {
 			
 		    fileScanner = new Scanner(inputFile); // Use the reading tool on the file.
 		    
-		    // While loop till file has line
+		    // Scan the input file for all integers and push eac integer to two lists.
+		    
 		    while (fileScanner.hasNextInt()) {
 		    	quickSortedArray[lineNumber] = fileScanner.nextInt(); // Read the line
 		    	shellSortedArray[lineNumber] = quickSortedArray[lineNumber++];
@@ -61,41 +82,41 @@ public class CyrilleLingaiJonathanGrant_06 {
 			fileScanner.close();
 			
 		} // End try.
-
 		catch (FileNotFoundException e) {
 			System.err.println(e);
 		} // End catch.
 		
 		// Sort both lists of integers.
 		
-		quickSort(quickSortedArray, 0, sequenceLength - 1);
 		shellSort(shellSortedArray);
+		quickSort(quickSortedArray, 0, sequenceLength - 1);
 		
-		// Attempt to find pre-existing file or create new one of specified name,
-		// and write the postfix expressions to file.
+		// Attempt to create new files of the specified names,
+		// and write the sorted lists to these files.
 		
 		try {
 			
+			// Write the shell sorted array to file.
+			
 			outputFileName = outputFileName.replace("X", Integer.toString(fileNumber));
 			fileWriter = new FileWriter(outputFileName);
-			fileWriter.write("Quick Sorted Array\n\n");
-			
-			fileWriter.write(arrayToString(quickSortedArray));;
-			
+			fileWriter.write("Shell Sorted Array\n\n");
+			fileWriter.write(arrayToString(shellSortedArray));
 			fileWriter.close();
+			
+			// Open a new file to write the quick sorted array.
 			
 			outputFileName = outputFileName.replace(
 					Integer.toString(fileNumber++), Integer.toString(fileNumber));
+			
+			// Write the quick sorted array to file.
+			
 			fileWriter = new FileWriter(outputFileName);
-			
-			fileWriter.write("Shell Sorted Array\n\n");
-			
-			fileWriter.write(arrayToString(shellSortedArray));
-			
+			fileWriter.write("Quick Sorted Array\n\n");
+			fileWriter.write(arrayToString(quickSortedArray));;
 			fileWriter.close();
 			
 		} // End try.
-		
 		catch (IOException e) {
 			System.err.println(e.getMessage());
 		}  // End catch.
@@ -105,13 +126,19 @@ public class CyrilleLingaiJonathanGrant_06 {
 // ****************************************************************************
 	
 	/**
-	 * Convert a sorted list of integers to a string with 10 integers per line for writing to file.
+	 * Convert a sorted list of integers to a string with 10 integers per line
+	 * for writing to file.
 	 * 
 	 * @param	sortedArray	The sorted list of integers.
 	 */
+	
 	private static String arrayToString(int[] sortedArray) {
 		
+		// Declaring local variables.
+		
 		String arrayString = "";
+		
+		// Pushing each integer of the sorted list to a string for the output file.
 		
 		for (int i = 0; i < sortedArray.length;) {
 			
@@ -134,6 +161,7 @@ public class CyrilleLingaiJonathanGrant_06 {
 	 * 
 	 * @param	unsortedArray	The list of unsorted integers to sort.
 	 */
+	
 	private static void shellSort(int[] unsortedArray) {
 		
 		// Declaring local variables.
@@ -142,15 +170,20 @@ public class CyrilleLingaiJonathanGrant_06 {
 		int index = 0;		// The current sublist of integers.
 		
 		// Iterate the list at intevals, dividing by 2 each time.
+		
 		for (int space = unsortedArray.length/2; space > 0; space /= 2) {
 	 
 			// Scan the sublist by each interval.
+			
 			for (int i = space; i < unsortedArray.length; i++) {
      
 				// Sort the sublist.
+				
 				nextInteger = unsortedArray[i];
 				
-				for (index = i; index >= space && unsortedArray[index - space] > nextInteger; index -= space) {
+				for (index = i; index >= space 
+						&& unsortedArray[index - space] > nextInteger; 
+						index -= space) {
 					
 					unsortedArray[index] = unsortedArray[index - space];
 					
@@ -170,30 +203,33 @@ public class CyrilleLingaiJonathanGrant_06 {
 	 * Quicksort implements the textbook case of quicksort to efficiently sort the
 	 * list by finding a pivot, swapping integers to the correct side of the pivot,
 	 * splitting each side of the pivot into sublists, and repeating until each
-	 * sublist has less than four elements.
+	 * sublist has less than four elements. Short sublists of less than four
+	 * integers are sorted using shell sort: an improved insertion sort.
 	 * 
 	 * @param 	anArray	The unsorted list of integers.
 	 */
+	
 	private static void quickSort(int[] anArray, int firstIndex, int lastIndex) {
 		
 		// Declaring local variables.
 		
 		int pivotIndex = 0;				// The pivot of the sublist.
 		final int MIN_SIZE = 4;			// The minimum size of the list.
-		int temp = 0;					// Index placeholder to swap two integers of the list.
-		int midIndex = (lastIndex - firstIndex) / 2; // Find the middle integer of the list.		
-		int leftIndex = firstIndex + 1;	// Integer to the left of the 
-		int rightIndex = lastIndex - 2;	// Right index of the sublist.
-		int pivotValue = 0;				// Left index of the sublist.
+		int temp = 0;					// Placeholder to swap two integers.
+		int midIndex = (lastIndex - firstIndex) / 2; 
+										// Find the middle integer of the list.		
+		int leftIndex = firstIndex + 1;	// Leftmost integer of the sublist.
+		int rightIndex = lastIndex - 2;	// Rightmostmost integer of the sublist.
+		int pivotValue = 0;				// The integer that seperates two sublists.
 		
-		// Sort the subarrays.
+		// Sort the sublist using insertion(shell) sort if the length is less than four,
+		// otherwise, sort the sublist by the quick sort algorithm.
 		
 		if (lastIndex - firstIndex < MIN_SIZE) {
 			
-			shellSort(anArray); // Sort the sublist using insertion sort if the length is less than three.
+			shellSort(anArray); 
 			
 		} // End if.
-		
 		else {
 			
 			// Exchange values of middle and last integers of sublist.
@@ -231,6 +267,8 @@ public class CyrilleLingaiJonathanGrant_06 {
 			anArray[pivotIndex] = anArray[leftIndex];
 			anArray[leftIndex] = temp;
 			pivotIndex = leftIndex;
+			
+			// Recursively quick sort the new sublists.
 			
 			quickSort(anArray, firstIndex, pivotIndex - 1);
 			quickSort(anArray, pivotIndex + 1, lastIndex);
